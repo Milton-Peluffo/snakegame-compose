16bd94962cac537408486e63395a15f5a01987f2
Milton-Peluffo <milton.peluffoc@gmail.com>
2025-12-02 21:21:34 -0500

    Style: some visual changes in colors

--- app/src/main/java/com/tomildev/snakegame_compose/ui/gameui/SnakeGameUI.kt
+++ app/src/main/java/com/tomildev/snakegame_compose/ui/gameui/SnakeGameUI.kt
@@ -5,14 +5,9 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -45,7 +40,7 @@ fun GameScreen(
         Box(
             modifier = Modifier
                 .size(width = gridWith, height = gridHeight)
-                .background(Color.LightGray)
+                .background(Color(0xFF9acc99))
                 .border(2.dp, color = Color.Black)
                 .clipToBounds()
         ){
@@ -69,7 +64,7 @@ fun snake(
                 y = (cellSize * position.y)
             )
             .size(cellSize)
-            .background(Color.Green)
+            .background(Color.Black)
             .border(2.dp, Color.Black)
 
     ) {
